38eb6135a833050b09b2c6c3eb120b46a53d8f14
shanafkhan <shanafkhan4@gmail.com>
2023-12-13 23:44:17 +0530

    number of students per class per subject

--- students_in_cl.js
+++ students_in_cl.js
@@ -1,109 +1,74 @@
-function allocateRooms(utilizedRooms, coursesCapacity) {
-    let utilizedRoomsCapacity = {};
-    let roomCoursesCapacity = {};
-    let roomEmptySeats = {};
-    let courseCapacityNotFilled = {};
-  
-    // Initialize utilizedRoomsCapacity
-    utilizedRooms.forEach((room) => {
-      utilizedRoomsCapacity[room] = 0;
-    });
-  
-    // Step 1
-    utilizedRooms.forEach((room) => {
-      roomCoursesCapacity[room] = [];
-      let totalRooms = utilizedRooms.length;
-  
-      for (let course in coursesCapacity) {
-        let j = Math.floor(coursesCapacity[course] / totalRooms);
-  
-        if (utilizedRoomsCapacity[room] >= j) {
-          roomCoursesCapacity[room].push(j);
-          utilizedRoomsCapacity[room] -= j;
-        } else {
-          if (utilizedRoomsCapacity[room] >= 0) {
-            roomCoursesCapacity[room].push(utilizedRoomsCapacity[room]);
-            utilizedRoomsCapacity[room] = 0;
+function allocateStudentsToRooms(coursesCapacity, utilizedRoomsCapacity) {
+  const roomCoursesCapacity = Array.from({ length: utilizedRoomsCapacity.length }, () => []);
+  const roomEmptySeats = new Array(utilizedRoomsCapacity.length).fill(0);
+  const courseCapacityNotFilled = new Array(coursesCapacity.length).fill(0);
+
+  for (let roomIndex = 0; roomIndex < utilizedRoomsCapacity.length; roomIndex++) {
+    const roomCapacity = utilizedRoomsCapacity[roomIndex];
+    for (let courseIndex = 0; courseIndex < coursesCapacity.length; courseIndex++) {
+      const j = Math.floor(coursesCapacity[courseIndex] / utilizedRoomsCapacity.length);
+      if (roomCapacity >= j) {
+        roomCoursesCapacity[roomIndex].push(j);
+        utilizedRoomsCapacity[roomIndex] -= j;
+      } else if (roomCapacity > 0) {
+        roomCoursesCapacity[roomIndex].push(roomCapacity);
+        utilizedRoomsCapacity[roomIndex] = 0;
+      } else {
+        roomCoursesCapacity[roomIndex].push(0);
+      }
+    }
+  }
+
+  for (let roomIndex = 0; roomIndex < utilizedRoomsCapacity.length; roomIndex++) {
+    if (roomCoursesCapacity[roomIndex].reduce((sum, value) => sum + value, 0) >= utilizedRoomsCapacity[roomIndex]) {
+      roomEmptySeats[roomIndex] = 0;
+    } else {
+      const difference =
+        utilizedRoomsCapacity[roomIndex] - roomCoursesCapacity[roomIndex].reduce((sum, value) => sum + value, 0);
+      roomEmptySeats[roomIndex] = difference;
+    }
+  }
+
+  for (let courseIndex = 0; courseIndex < coursesCapacity.length; courseIndex++) {
+    if (
+      coursesCapacity[courseIndex] ===
+      roomCoursesCapacity.reduce((sum, roomCapacityList) => sum + roomCapacityList[courseIndex], 0)
+    ) {
+      courseCapacityNotFilled[courseIndex] = 0;
+    } else {
+      const difference =
+        coursesCapacity[courseIndex] -
+        roomCoursesCapacity.reduce((sum, roomCapacityList) => sum + roomCapacityList[courseIndex], 0);
+      courseCapacityNotFilled[courseIndex] = difference;
+    }
+  }
+
+  for (let courseIndex = 0; courseIndex < coursesCapacity.length; courseIndex++) {
+    if (courseCapacityNotFilled[courseIndex] !== 0) {
+      for (let roomIndex = 0; roomIndex < utilizedRoomsCapacity.length; roomIndex++) {
+        if (roomEmptySeats[roomIndex] !== 0) {
+          if (roomEmptySeats[roomIndex] >= courseCapacityNotFilled[courseIndex]) {
+            roomEmptySeats[roomIndex] -= courseCapacityNotFilled[courseIndex];
+            roomCoursesCapacity[roomIndex][courseIndex] += courseCapacityNotFilled[courseIndex];
+            courseCapacityNotFilled[courseIndex] = 0;
           } else {
-            roomCoursesCapacity[room].push(0);
+            courseCapacityNotFilled[courseIndex] -= roomEmptySeats[roomIndex];
+            roomCoursesCapacity[roomIndex][courseIndex] += roomEmptySeats[roomIndex];
+            roomEmptySeats[roomIndex] = 0;
           }
         }
-      }
-    });
-  
-    // Step 2
-    utilizedRooms.forEach((room) => {
-      if (roomCoursesCapacity[room].reduce((sum, value) => sum + value, 0) >= utilizedRoomsCapacity[room]) {
-        roomEmptySeats[room] = 0;
-      } else {
-        let difference = utilizedRoomsCapacity[room] - roomCoursesCapacity[room].reduce((sum, value) => sum + value, 0);
-        roomEmptySeats[room] = difference;
-      }
-    });
-  
-    // Step 3
-    for (let course in coursesCapacity) {
-      if (coursesCapacity[course] === utilizedRooms.reduce((sum, room) => sum + roomCoursesCapacity[room][course], 0)) {
-        courseCapacityNotFilled[course] = 0;
-      } else {
-        let difference = coursesCapacity[course] - utilizedRooms.reduce((sum, room) => sum + roomCoursesCapacity[room][course], 0);
-        courseCapacityNotFilled[course] = difference;
-      }
-    }
-  
-    // Step 4
-    for (let course in coursesCapacity) {
-      if (courseCapacityNotFilled[course] !== 0) {
-        for (let room of utilizedRooms) {
-          if (roomEmptySeats[room] !== 0) {
-            if (roomEmptySeats[room] >= courseCapacityNotFilled[course]) {
-              roomEmptySeats[room] -= courseCapacityNotFilled[course];
-              roomCoursesCapacity[room][course] += courseCapacityNotFilled[course];
-              courseCapacityNotFilled[course] = 0;
-            } else {
-              courseCapacityNotFilled[course] -= roomEmptySeats[room];
-              roomCoursesCapacity[room][course] += roomEmptySeats[room];
-              roomEmptySeats[room] = 0;
-            }
-          }
-          if (courseCapacityNotFilled[course] === 0) {
-            break;
-          }
+        if (courseCapacityNotFilled[courseIndex] === 0) {
+          break;
         }
       }
     }
-  
-    // Remove NaN values
-    roomCoursesCapacity = removeNaNValues(roomCoursesCapacity);
-    roomEmptySeats = removeNaNValues(roomEmptySeats);
-    courseCapacityNotFilled = removeNaNValues(courseCapacityNotFilled);
-  
-    return {
-      roomCoursesCapacity,
-      roomEmptySeats,
-      courseCapacityNotFilled,
-    };
   }
-  
-  // Helper function to remove NaN values from objects
-  function removeNaNValues(obj) {
-    let result = {};
-    for (let key in obj) {
-      result[key] = obj[key].map(value => isNaN(value) ? 0 : value);
-    }
-    return result;
-  }
-  
-  // Example usage:
-  const utilizedRooms = ['Room1', 'Room2', 'Room3'];
-  const coursesCapacity = {
-    Course1: 30,
-    Course2: 20,
-    Course3: 25,
-  };
-  
-  const result = allocateRooms(utilizedRooms, coursesCapacity);
-  console.log(result.roomCoursesCapacity);
-  console.log(result.roomEmptySeats);
-  console.log(result.courseCapacityNotFilled);
-  
+
+  return roomCoursesCapacity;
+}
+
+// Example usage
+const coursesCapacity = [30, 40, 25];
+const utilizedRoomsCapacity = [100, 80, 120];
+const result = allocateStudentsToRooms(coursesCapacity, utilizedRoomsCapacity);
+console.log(result);
